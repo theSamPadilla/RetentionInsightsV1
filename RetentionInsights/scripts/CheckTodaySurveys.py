7ac5efb715ef21771960fb1d23cbc86a1f5305ab
root <root@spring2020.us-central1-a.c.propane-cubist-301618.internal>
2021-03-18 15:44:44 +0000

    Creating a script to check today's responses and surveys.

---
+++ RetentionInsights/scripts/CheckTodaySurveys.py
@@ -0,0 +1,37 @@
+## SETUP TO RUN THE SCRIPT ##
+import os, sys
+path = '/home/sam/RetentionInsightsV1/RetentionInsights/'
+sys.path.append(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RetentionInsights.settings.production")
+
+import django
+django.setup()
+#############################
+
+"""
+
+DESCRIPTION:
+This script checks the completed surveys and recorded responses for today.
+
+USAGE:
+To check toda's survey.
+
+"""
+
+# My imports #
+from surveys.models import Survey, Response #type: ignore
+
+# Grab all surveys for today
+todaySurveys = Survey.objects.filter(creationDate__gte = '2021-03-17', userID__studyID = 2)
+
+# Grab completed surveys
+completed = todaySurveys.filter(completed_p = True)
+
+# Grab all responses for today
+responses = Response.objects.filter(questionID__surveyID__creationDate__gte = '2021-03-17',
+            questionID__surveyID__completed_p = True, 
+            questionID__surveyID__userID__studyID = 2)
+
+print ("\nToday Surveys for Morningside College: %d" % len(todaySurveys))
+print ("Completed Surveys today: %d" % len(completed))
+print ("Recroded Responses today: %d" % len(responses))
